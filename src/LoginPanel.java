0e1b22c3a96d33756acf842d64885c749c470955
lorenamitoi <32674498+lorenamitoi@users.noreply.github.com>
2019-06-27 07:49:46 +0100

    main frame

--- src/LoginPanel.java
+++ src/LoginPanel.java
@@ -2,5 +2,19 @@ import javax.swing.*;
 
 public class LoginPanel extends JPanel
 {
+    private JLabel loginLabel;
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JLabel userLabel;
+    private JLabel passwordLabel;
+    private JButton loginB;
 
+    public LoginPanel()
+    {
+        loginLabel = new JLabel("Enter your JPMChase credentials: ");
+        usernameField = new JTextField(15);
+        passwordField = new JPasswordField(15);
+        userLabel = new JLabel("ID: ");
+        passwordLabel = new JLabel("Pass:");
+    }
 }
